914e5004831dcc07134da7ce0b0032ad333a3d83
Administrator <451367682@qq.com>
2017-10-20 18:48:50 +0800

    ...

---
+++ src/test/java/Main.java
@@ -0,0 +1,8 @@
+import java.util.Calendar;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(Calendar.getInstance().get(Calendar.YEAR));
+		System.out.println(Calendar.getInstance().get(Calendar.MONTH)+1);
+	}
+}
